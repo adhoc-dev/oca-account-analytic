8ecf039547f55163bac17d6dfc763bcafb6387ca
mav-adhoc <mav@adhoc.com.ar>
2025-10-28 17:04:50 -0300

    [18.0][FIX]stock_landed_costs_analytic

--- stock_landed_costs_analytic/models/stock_landed_cost_lines.py
+++ stock_landed_costs_analytic/models/stock_landed_cost_lines.py
@@ -1,9 +1,17 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, fields
 
 
 class StockLandedCostLines(models.Model):
     _name = "stock.landed.cost.lines"
     _inherit = ["stock.landed.cost.lines", "analytic.mixin"]
+
+    company_id = fields.Many2one(
+        'res.company',
+        string='Company',
+        related='cost_id.company_id',
+        store=True,
+        readonly=True
+    )
